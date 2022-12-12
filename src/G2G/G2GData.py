209d9f8c356ee585ea9df5a94a55840b19bcd5cc
dinispimentel <dinisnerd@gmail.com>
2022-12-12 22:39:56 -0100

    commenting useful abs paths

--- src/G2G/G2GData.py
+++ src/G2G/G2GData.py
@@ -9,6 +9,8 @@ class G2GData:
     def __init__(self, offerbook: OfferBook = None, lastUpdate=None, status=None, progress=None):
         self.locked: bool = False
         self.offerbook: OfferBook = offerbook or OfferBook.loadFromFile("/home/dp/Desktop/Eco-G2GSDB/src/cache/myofferbook.json")
+        # OfferBook.loadFromFile("/home/dp/Desktop/Eco-G2GSDB/src/cache/myofferbook.json")
+        # OfferBook.writeToFile("/home/dp/Desktop/Eco-G2GSDB/src/cache/myofferbook.json")
         self.lastUpdate: float = lastUpdate
         self.status: str = status or "Idle"
         # self.progress: Callable[[float, int], bool] = progress  # em ratio [0 ; 1]
