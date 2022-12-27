2cf1cd726ee8d12cfb08259cbb36222b7f9e1ac2
dinispimentel <dinisnerd@gmail.com>
2022-12-27 17:15:16 -0100

    pycache remove; G2GData trivial change

--- src/G2G/G2GData.py
+++ src/G2G/G2GData.py
@@ -10,7 +10,9 @@ class G2GData:
         self.locked: bool = False
         self.offerbook: OfferBook = offerbook or OfferBook.loadFromFile("/home/dp/Desktop/Eco-G2GSDB/src/cache/myofferbook.json")
         # OfferBook.loadFromFile("/home/dp/Desktop/Eco-G2GSDB/src/cache/myofferbook.json")
-        # OfferBook.writeToFile("/home/dp/Desktop/Eco-G2GSDB/src/cache/myofferbook.json")
+        #
+
+
         self.lastUpdate: float = lastUpdate
         self.status: str = status or "Idle"
         # self.progress: Callable[[float, int], bool] = progress  # em ratio [0 ; 1]
